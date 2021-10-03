a8caec771730aea19a9a13a2aee6bdd1fa0ec35c
gabriel.mcamargo <gabrielcamargo1409@gmail.com>
2021-10-03 15:12:47 -0300

    sidebar links

--- apps/front/src/components/Sidebar/index.tsx
+++ apps/front/src/components/Sidebar/index.tsx
@@ -1,12 +1,24 @@
-import { Flex } from "@chakra-ui/layout"
-import { VStack } from "@chakra-ui/react"
+import { Flex, Link } from "@chakra-ui/layout";
+import { VStack } from "@chakra-ui/react";
+import { ActiveLink } from "./ActiveLink";
 
-export const Sidebar = () =>{
-    return(
-        <Flex h="100vh" w={100} bg="gray.900">
-            <VStack as="ul" spacing="6">
-  
-            </VStack>
-        </Flex>
-    )
-}
+export const Sidebar = () => {
+  return (
+    <Flex h="100vh" w={100} bg="gray.900">
+      <VStack as="ul" spacing="6">
+        <ActiveLink href="/dashboard" passHref>
+          <Link>Map</Link>
+        </ActiveLink>
+        <ActiveLink href="/map" passHref>
+          <Link>Map</Link>
+        </ActiveLink>
+        <ActiveLink href="/map" passHref>
+          <Link>Map</Link>
+        </ActiveLink>
+        <ActiveLink href="/map" passHref>
+          <Link>Map</Link>
+        </ActiveLink>
+      </VStack>
+    </Flex>
+  );
+};
