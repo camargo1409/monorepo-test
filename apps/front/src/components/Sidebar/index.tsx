522c35e5650e67aebd2d18f3b226bee2464323b2
gabriel.mcamargo <gabrielcamargo1409@gmail.com>
2021-10-03 14:45:46 -0300

    dashboard structure

---
+++ apps/front/src/components/Sidebar/index.tsx
@@ -0,0 +1,12 @@
+import { Flex } from "@chakra-ui/layout"
+import { VStack } from "@chakra-ui/react"
+
+export const Sidebar = () =>{
+    return(
+        <Flex h="100vh" w={100} bg="gray.900">
+            <VStack as="ul" spacing="6">
+  
+            </VStack>
+        </Flex>
+    )
+}
