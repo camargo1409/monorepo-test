6c2ad3154d91e80fa891d1a1e5fc6efe96716e30
gabriel.mcamargo <gabrielcamargo1409@gmail.com>
2021-11-06 14:44:25 -0300

    fix: updates

--- apps/back/app/Controllers/Http/RequestController.js
+++ apps/back/app/Controllers/Http/RequestController.js
@@ -1,67 +1,99 @@
-'use strict'
-const Request = use('App/Models/Request')
+"use strict";
+const Request = use("App/Models/Request");
 class RequestController {
-    async index ({ request, response, auth }) {
-        const { as } = request.get()
-        
-        const { id } = await auth.getUser()
-        
-        if(as === "customer"){
-            const requests = await Request.query().where('customer_id',id).with('provider').fetch()
-            return response.json(requests)
-        }
+  async index({ request, response, auth }) {
+    const { as } = request.get();
 
-        if(as === "provider"){
-            const requests = await Request.query().where('post_provider_id',id).with('customer').fetch()
-            return response.json(requests)
-        }
+    const { id } = await auth.getUser();
 
-        return response.json(await Request.all())
-
+    if (as === "customer") {
+      const requests = await Request.query()
+        .where("customer_id", id)
+        .with("provider")
+        .fetch();
+      return response.json(requests);
     }
 
-    async store({request,response,auth}){
-        const {post_provider_id} = request.all()
-        const {id:customer_id} = await auth.getUser()
+    if (as === "provider") {
+      const requests = await Request.query()
+        .where("post_provider_id", id)
+        .with("customer")
+        .fetch();
+      return response.json(requests);
+    }
 
-        if(customer_id === post_provider_id){
-            return response.status(400).json({msg:"customer and provider are the same users"})
-        }
+    return response.json(await Request.all());
+  }
 
-        try {
-            const req = await Request.create({customer_id,post_provider_id})
-            return response.status(200).json(req)
-        } catch (error) {
-            return response.status(400).json({msg:error.sqlMessage})
-        }
+  async store({ request, response, auth }) {
+    const { post_provider_id } = request.all();
+    const { id: customer_id } = await auth.getUser();
+
+    const getOpenRequests = await Request.query()
+      .where({
+        post_provider_id,
+        customer_id,
+        provider_accepted: false,
+      })
+      .fetch()
+
+    const isAlreadyOpenRequest = getOpenRequests.toJSON();
+
+    if (customer_id === post_provider_id) {
+      return response
+        .status(400)
+        .json({ msg: "customer and provider are the same users" });
+    }
+
+    if (isAlreadyOpenRequest.length > 0) {
+      return response
+        .status(403)
+        .json({ msg: "there is already an open request with this provider" });
+    }
+
+    try {
+      const req = await Request.create({ customer_id, post_provider_id });
+      return response.status(200).json(req);
+    } catch (error) {
+      return response.status(400).json({ msg: error.sqlMessage });
+    }
+  }
+
+  async update({ request, response, auth, params }) {
+    const { id } = params;
+    const { id: user_id } = await auth.getUser();
+    const { action } = request.get();
+
+    const req = await Request.findOrFail(id);
+
+    if (action === "accept") {
+      if (req.post_provider_id !== user_id) {
+        return response
+          .status(401)
+          .json({ msg: "you aren't the provider of the request" });
+      }
+
+      const { service_price } = request.post();
+
+      if (!service_price)
+        return response
+          .status(400)
+          .json({ msg: "provide the price of service" });
+
+      req.service_price = service_price;
+      req.provider_accepted = true;
+
+      const updated = await req.save();
+
+      return response.status(200).json(updated);
+    }
+
+    if(action === "hire_provider"){
 
     }
     
-    async update({request,response,auth,params}){
-        const {id} = params
-        const {id: user_id} = await auth.getUser()
-        const {action} = request.get()
-
-        const req = await Request.findOrFail(id)
-
-        if(action === "accept"){
-            if(req.post_provider_id !== user_id){
-                return response.status(401).json({msg:"you aren't the provider of the request"})
-            }
-
-            const {service_price} = request.post()
-
-            if(!service_price) return response.status(400).json({msg:"provide the price of service"})
-
-            req.service_price = service_price
-            req.provider_accepted = true
-
-            const updated = await req.save()
-
-            return response.status(200).json(updated)
-        }
-        return response.json({id,action,req})
-    }
+    return response.json({ id, action, req });
+  }
 }
 
-module.exports = RequestController
+module.exports = RequestController;
--- apps/back/app/Controllers/Http/SearchController.js
+++ apps/back/app/Controllers/Http/SearchController.js
@@ -3,8 +3,7 @@ const User = use('App/Models/User')
 class SearchController {
     async index({request, response, auth}){
         const { id,lat, long } = await auth.getUser()
-        console.log(lat,long);
-        const usersNearByMe = await User.query().whereNot({id:id}).where({available:false}).nearBy(lat,long,700).fetch()
+        const usersNearByMe = await User.query().whereNot({id:id}).where({available:true}).nearBy(lat,long,10).fetch()
 
         return response.json(usersNearByMe)
     }
