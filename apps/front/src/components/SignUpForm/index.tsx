15def721ff6b4949dc8b3003462e95ab68340e40
gabriel.mcamargo <gabrielcamargo1409@gmail.com>
2022-06-06 20:04:05 -0300

    chore: add cellphone field on signup form

--- apps/front/src/components/SignUpForm/index.tsx
+++ apps/front/src/components/SignUpForm/index.tsx
@@ -19,6 +19,7 @@ import {
   ListIcon,
   OrderedList,
   UnorderedList,
+  Checkbox,
 } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/input";
 import { Button } from "@chakra-ui/button";
@@ -53,6 +54,7 @@ interface SignUpFormData {
   state: string;
   city: string;
   available: boolean;
+  cellphone: string;
 }
 
 interface SignUpFormProps {
@@ -66,6 +68,7 @@ interface SignUpFormProps {
     state: string;
     city: string;
     available: boolean;
+    cellphone: string;
   };
 }
 
@@ -80,13 +83,13 @@ export const SignUpForm = ({ labelColor, fillForm }: SignUpFormProps) => {
 
   const { position } = useContext(MapContext);
 
-  const router = useRouter()
+  const router = useRouter();
 
   const handleSignUp: SubmitHandler<SignUpFormData> = async (values) => {
     try {
       const { confirm_password, ...rest } = values;
 
-      const {email, password} = rest
+      const { email, password } = rest;
 
       const { data } = await api.post("/users", {
         ...rest,
@@ -152,19 +155,30 @@ export const SignUpForm = ({ labelColor, fillForm }: SignUpFormProps) => {
               />
             </FormControl>
           </HStack>
-
+          <FormControl mt={8}>
+            <FormLabel color={labelColor} htmlFor="email">
+              Email
+            </FormLabel>
+            <Input
+              color={labelColor}
+              value={fillForm?.email}
+              id="email"
+              variant="flushed"
+              type="text"
+              {...register("email")}
+            />
+          </FormControl>
           <HStack mt={8}>
             <FormControl>
               <FormLabel color={labelColor} htmlFor="email">
-                Email
+                Celular
               </FormLabel>
               <Input
                 color={labelColor}
-                value={fillForm?.email}
-                id="email"
+                value={fillForm?.cellphone}
+                id="cellphone"
                 variant="flushed"
-                type="email"
-                {...register("email")}
+                {...register("cellphone")}
               />
             </FormControl>
             <FormControl>
@@ -286,11 +300,18 @@ export const SignUpForm = ({ labelColor, fillForm }: SignUpFormProps) => {
         </FormControl>
 
         <FormControl display="flex">
-          <FormLabel color={labelColor} htmlFor="available" mb="0" fontWeight="normal">
-            Ao entrar, você esta concordando com nossa{" "}
-            <Link color="teal.500" onClick={onOpen}>
-              Política de Privacidade e Termos de Uso
-            </Link>
+          <FormLabel
+            color={labelColor}
+            htmlFor="available"
+            mb="0"
+            fontWeight="normal"
+          >
+            <Checkbox colorScheme="pink">
+              Ao entrar, você esta concordando com nossa{" "}
+              <Link color="teal.500" onClick={onOpen}>
+                Política de Privacidade e Termos de Uso
+              </Link>
+            </Checkbox>
           </FormLabel>
         </FormControl>
 
@@ -302,7 +323,12 @@ export const SignUpForm = ({ labelColor, fillForm }: SignUpFormProps) => {
           >
             Salvar
           </Button>
-          <Button colorScheme="gray" onClick={() => router.push('/auth/signin')}>Voltar</Button>
+          <Button
+            colorScheme="gray"
+            onClick={() => router.push("/auth/signin")}
+          >
+            Voltar
+          </Button>
         </Flex>
       </VStack>
       <Modal isOpen={isOpen} size="xl" onClose={onClose}>
@@ -312,34 +338,79 @@ export const SignUpForm = ({ labelColor, fillForm }: SignUpFormProps) => {
           <ModalCloseButton />
           <ModalBody>
             <p>
-              A sua privacidade é importante para nós. É política do BeTheBox respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site BeTheBox, e outros sites que possuímos e operamos.
-            </p><br />
+              A sua privacidade é importante para nós. É política do BeTheBox
+              respeitar a sua privacidade em relação a qualquer informação sua
+              que possamos coletar no site BeTheBox, e outros sites que
+              possuímos e operamos.
+            </p>
+            <br />
             <p>
-              Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemos por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.
-            </p><br />
+              Solicitamos informações pessoais apenas quando realmente
+              precisamos delas para lhe fornecer um serviço. Fazemos por meios
+              justos e legais, com o seu conhecimento e consentimento. Também
+              informamos por que estamos coletando e como será usado.
+            </p>
+            <br />
             <p>
-              Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis para evitar perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não autorizados.
-            </p><br />
+              Apenas retemos as informações coletadas pelo tempo necessário para
+              fornecer o serviço solicitado. Quando armazenamos dados,
+              protegemos dentro de meios comercialmente aceitáveis para evitar
+              perdas e roubos, bem como acesso, divulgação, cópia, uso ou
+              modificação não autorizados.
+            </p>
+            <br />
             <p>
-              Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.
-            </p><br />
+              Não compartilhamos informações de identificação pessoal
+              publicamente ou com terceiros, exceto quando exigido por lei.
+            </p>
+            <br />
             <p>
-              O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas políticas de privacidade.
-            </p><br />
+              O nosso site pode ter links para sites externos que não são
+              operados por nós. Esteja ciente de que não temos controle sobre o
+              conteúdo e práticas desses sites e não podemos aceitar
+              responsabilidade por suas respectivas políticas de privacidade.
+            </p>
+            <br />
             <p>
-              Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados.
-            </p><br />
+              Você é livre para recusar a nossa solicitação de informações
+              pessoais, entendendo que talvez não possamos fornecer alguns dos
+              serviços desejados.
+            </p>
+            <br />
             <p>
-              O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contacto conosco.
-            </p><br />
-            <Heading as='h4' size='md'>Compromisso do Usuário</Heading>
-            <p>O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o BeTheBox oferece no site e com caráter enunciativo, mas não limitativo:</p><br />
+              O uso continuado de nosso site será considerado como aceitação de
+              nossas práticas em torno de privacidade e informações pessoais. Se
+              você tiver alguma dúvida sobre como lidamos com dados do usuário e
+              informações pessoais, entre em contacto conosco.
+            </p>
+            <br />
+            <Heading as="h4" size="md">
+              Compromisso do Usuário
+            </Heading>
+            <p>
+              O usuário se compromete a fazer uso adequado dos conteúdos e da
+              informação que o BeTheBox oferece no site e com caráter
+              enunciativo, mas não limitativo:
+            </p>
+            <br />
             <UnorderedList>
-              <ListItem>Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;</ListItem>
-              <ListItem>Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, Onde Bola ou azar, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;</ListItem>
-              <ListItem>Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do BeTheBox, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.</ListItem>
+              <ListItem>
+                Não se envolver em atividades que sejam ilegais ou contrárias à
+                boa fé a à ordem pública;
+              </ListItem>
+              <ListItem>
+                Não difundir propaganda ou conteúdo de natureza racista,
+                xenofóbica, Onde Bola ou azar, qualquer tipo de pornografia
+                ilegal, de apologia ao terrorismo ou contra os direitos humanos;
+              </ListItem>
+              <ListItem>
+                Não causar danos aos sistemas físicos (hardwares) e lógicos
+                (softwares) do BeTheBox, de seus fornecedores ou terceiros, para
+                introduzir ou disseminar vírus informáticos ou quaisquer outros
+                sistemas de hardware ou software que sejam capazes de causar
+                danos anteriormente mencionados.
+              </ListItem>
             </UnorderedList>
-          
           </ModalBody>
 
           <ModalFooter>
